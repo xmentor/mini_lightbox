2d92c9873a0328e04de37812d7aae23a7d03b13d
Kamil Kondratowicz <prywatny3@o2.pl>
2017-03-08 19:45:12 +0100

    Version based on functions
    Version based on functions, rename classes, addition 'cancel' img,
    improve accessibility, change structure directories/files

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# mini_lightbox
-[Demo](https://xmentor.github.io/mini_lightbox/)
+# Mini Lightbox
+
+[Wersja demonstracyjna](https://xmentor.github.io/mini_lightbox/)
+
+## Użycie
+
+* Pobierz [pliki](https://github.com/xmentor/mini_lightbox/archive/functions.zip) - wersja oparta na funkcjach
+
+* Foldery wraz z plikami, które znajdują się w `dest` przenieś do swojego katalogu.
+
+* Style zalinkuj w sekcji head:
+
+    ```html
+    <head>
+        <!-- Metadane -->
+        <link rel="stylesheet" href="css/lightbox.css">
+        <!-- Twoje style -->
+    </head>
+    ```
+    
+* Skrypt podepnij tuż przed zamknięciem znacznika `body`:
+
+    ```html
+    <body>
+        <!-- Znaczniki -->
+        <!-- Inne skrypty -->
+        <script src="js/lightbox.min.js></script>
+    </body>
+    ```
+
+## Licencja
+
+[Licencja MIT](https://github.com/xmentor/mini_lightbox/blob/master/LICENSE)
---
+++ src/js/lightbox.js
@@ -0,0 +1,122 @@
+(function() {
+    'use strict';
+    
+    let activedElement;
+   
+    const getWindowDimensions = () => {
+        const width = window.innerWidth - 50;
+        const height = window.innerHeight - 100;
+        
+        return {width, height};
+    };
+    
+    const createLightBox = (src, alt, maxWidth, maxHeight) => {
+        const lightBox = document.createElement('div');
+        
+        lightBox.classList.add('lightbox');
+        
+        lightBox.innerHTML = `<div class='lightbox__container' role='dialog' aria-labelledby='lb-img' aria-describedby='lb-info' tabindex='-1'>
+                                <button class='lightbox__close close'>
+                                    <img src='lb-img/close.png' alt='Zamknij okno' class='close__img'>
+                                </button>
+                                <img src='${src}' alt='${alt}' style='max-width: ${maxWidth}px; max-height: ${maxHeight}px;' class='lightbox__img' id='lb-img'>
+                                <p class='lightbox__info' id='lb-info'>
+                                    <span class='visuallyhidden'>Otworzono powiększenie zdjęcia. Naciśnij <kbd>ESC</kbd> lub <kbd>TAB</kbd>, aby zamknąć powiększenie.</span>
+                                </p>
+                              </div>`;
+        
+        return lightBox;
+    };
+    
+    const showLightBox = () => {
+        const lightBox = document.querySelector('.lightbox');
+        const containerLightBox = document.querySelector('.lightbox__container');
+        const imageLightBox = document.querySelector('.lightbox__img');
+        const showBox = () => {
+            lightBox.classList.add('fadeIn');
+            setTimeout(() => {
+                containerLightBox.classList.add('fadeIn');
+                containerLightBox.focus();
+                imageLightBox.classList.add('fadeIn');
+            }, 200);
+        };
+        
+        if(imageLightBox.complete) {
+            showBox();
+        } else {
+            imageLightBox.addEventListener('load', showBox);
+        }
+        document.addEventListener('click', clickHandler);
+        document.addEventListener('keydown', keyHandler);
+    };
+    
+    const removeLightBox = () => {
+        const lightBox = document.querySelector('.lightbox');
+        
+        lightBox.classList.remove('fadeIn');
+        setTimeout(() => {
+            lightBox.parentNode.removeChild(lightBox);
+        }, 200);
+        activedElement.focus();
+    };
+    
+    const removeEvents = () => {
+        document.removeEventListener('click', clickHandler);
+        document.removeEventListener('keydown', keyHandler);
+    };
+    
+    const clickHandler = (e) => {
+        const target = e.target;
+        if(target.classList.contains('lightbox__img')) {
+            return false;
+        }
+        
+        // e.stopPropagation();
+        e.preventDefault();
+        
+        removeEvents();
+        removeLightBox();
+    };
+    
+    const keyHandler = (e) => {
+        const tabKeyCode = 9;
+        const escKeyCode = 27;
+        if(e.keyCode !== escKeyCode && e.keyCode !== tabKeyCode) {
+            return false;
+        }
+        
+        // e.stopPropagation();
+        e.preventDefault();
+        
+        removeEvents();
+        removeLightBox();
+    };
+    
+    const appendToBody = (element) => {
+        document.body.appendChild(element);
+        showLightBox();
+    };
+    
+    const newLightBox = (srcImg, dataAlt = 'Screenshoot') => {
+        const {width, height} = getWindowDimensions();
+        const lightBox = createLightBox(srcImg, dataAlt, width, height);
+        
+        appendToBody(lightBox);
+    };
+
+    for(let thumbnails = document.querySelectorAll('[data-lightbox]'), thumnailsLen = thumbnails.length, i = 0; i < thumnailsLen; i++) {
+        thumbnails[i].addEventListener('click', function(e) {
+            const href = this.href;
+            if(!href || !/\.(gif|jpg|jpeg|tiff|png|bmp)$/i.test(href)) {
+                return false;
+            }
+             
+            e.preventDefault();
+            e.stopPropagation();
+             
+            activedElement = this;
+             
+            newLightBox(href, this.dataset.alt);
+        });
+    }
+}());

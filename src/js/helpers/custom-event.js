786a954041b0e247992b33afa28e0f47fefd482c
Kamil Kondratowicz <kamil.kondratowicz@owlsdepartment.com>
2019-03-23 20:00:55 +0100

    removed useless files & added  dist

--- src/js/helpers/custom-event.js
+++
@@ -1,7 +0,0 @@
-const customEvent = (element, eventName) => {
-    const event = document.createEvent('Event');
-
-    return event.initEvent(eventName, false, false);
-};
-
-export default customEvent;
